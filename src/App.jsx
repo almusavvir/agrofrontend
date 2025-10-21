57cd08fd86c8fdc44376b913e85c8632050bc95c
almusavvir/devbase <almusavvir.siddiqui@gmail.com>
2025-10-21 15:35:51 +0530

    initial app structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserCard from './components/UserCard'
+
+function App() {
+
+  return (
+   <>
+   <UserCard />
+   <UserCard />
+   </>  
+  )
+}
+
+export default App
